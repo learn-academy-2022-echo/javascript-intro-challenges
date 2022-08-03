a87fa1922da7d72f21fccc6952342bab459daad6
dnlclmnt <dnlclmnt@gmail.com>
2022-08-03 11:36:54 -0700

    completed second question

--- questions-vanessa-daniel.js
+++ questions-vanessa-daniel.js
@@ -3,6 +3,9 @@
 // Write the code that will log the outcome of 34 added to 71.
     console.log(34 + 71)
 // Write the code that will log the outcome of 67 subtracted from 123.
+    let firstNumber = 123
+    let secondNumber = 67
+    console.log(firstNumber - secondNumber)
 // Write the code that will log the outcome of 56 multiplied by 23.
 // Write the code that will log the outcome of 45 divided by 5.
 // Write the code that will log the outcome of 5 to the power of 7.
