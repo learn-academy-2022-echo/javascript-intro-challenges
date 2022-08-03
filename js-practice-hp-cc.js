bc0998ddc9c58752e1a0d6bd3dff601960107d3b
CathrineC <cjsr.carreon@gmail.com>
2022-08-03 12:02:25 -0700

    2nd Part of Challeng - Done

--- js-practice-hp-cc.js
+++ js-practice-hp-cc.js
@@ -20,15 +20,28 @@ console.log(myName.includes("e"))
 console.log(myName.charAt(0))
 // Write the code that will log the string in all uppercase letters.
 console.log(myName.toUpperCase())
+
+
 // Write the code that will log true or false for the following:
 
 // Is 34 divided by 3 greater than 67 divided by 2?
+console.log(34 / 3 > 67 / 2)
 // Does 5 evaluate to the same as "5"?
+console.log(5 === "5")
 // Does 5 strictly equal "5"?
+console.log(5 === "5")
 // Does !3 strictly equal 3?
+console.log(!3 === 3)
 // Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+console.log("LEARN".length === 5 && "Student".length === 7)
 // Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+console.log("LEARN".length === 5 || "Student".length === 10)
 // Does "LEARN" contain the subset "RN"?
+var myCohort = "LEARN"
+console.log(myCohort.includes("RN"))
 // Does "LEARN" contain the subset "rn"?
+console.log(myCohort.includes("rn"))
 // Does "LEARN"[0] strictly equal "l"?
+console.log(myCohort.charAt(0) === "l")
 // Modify the code from the previous question to return true.
+console.log(myCohort.charAt(0) === "L")
