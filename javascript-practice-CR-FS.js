aab0bbfd23b2a130e1ea6ad66720e87396525dc9
roeckerc <roeckerc@gmail.com>
2022-08-03 13:06:33 -0700

    your turn

--- javascript-practice-CR-FS.js
+++ javascript-practice-CR-FS.js
@@ -1,2 +1,10 @@
-hey chris, this is our javascript practice, how we doing?
-hello we did it 
+//hey chris, this is our javascript practice, how we doing?
+//hello we did it 
+console.log(34+71)
+console.log(64 - 123)
+console.log(56 *23)
+console.log(45 / 5)
+console.log(5 ** 7)
+console.log(33 % 6)
+console.log("cliff".length)
+
