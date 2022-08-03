120a7c3f7b5682c7038aad253e17553ac953096e
jmagistrado <joycemagistrado@gmail.com>
2022-08-03 11:39:02 -0700

    Here is our answers

---
+++ challenges-ll-jm.js
@@ -0,0 +1,21 @@
+// Write the code that will log the outcome of 34 added to 71.
+console.log(34 + 71)
+// Write the code that will log the outcome of 67 subtracted from 123.
+console.log(67 - 123)
+// Write the code that will log the outcome of 56 multiplied by 23.
+console.log(56 * 23)
+// Write the code that will log the outcome of 45 divided by 5.
+console.log(45 / 5)
+// Write the code that will log the outcome of 5 to the power of 7.
+console.log(5 ** 7)
+// Write the code that will log the whole number remainder of 33 divided by 6.
+console.log(33 % 6)
+// Write the code that will log the length of a string containing your name.
+console.log("joyce".length)
+// Write the code that will log whether your string includes the letter "e"?
+var myName = "joyce"
+console.log(myName.includes("e"))
+// Write the code that will log the character at the first index of the string.
+console.log(myName.indexOf("j"))
+// Write the code that will log the string in all uppercase letters.
+console.log(myName.toUpperCase())
