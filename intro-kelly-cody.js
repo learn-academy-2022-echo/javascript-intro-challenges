020b4695bced162564fb0721a668e8d10b037460
kellychan56 <kelly.chan568@gmail.com>
2022-08-03 11:59:54 -0700

    we're pros

--- intro-kelly-cody.js
+++ intro-kelly-cody.js
@@ -38,3 +38,17 @@ console.log("LEARN".includes("rn"));
 console.log("LEARN"[0] === "l");
 // Modify the code from the previous question to return true.
 console.log("LEARN"[0] === "L")
+
+var theQuestion = "life, the universe, and everything";
+var theAnswer = 42;
+// Write the code that will log theAnswer divided by 2.
+console.log(theAnswer / 2);
+// Write the code that will log the length of theQuestion.
+console.log(theQuestion.length);
+// Write the code that will log the index of the character "f" in the theQuestion.
+console.log(theQuestion.indexOf("f"));
+// Write the code that will log the concatenation of the two variables.
+console.log(theQuestion + theAnswer);
+// Write the code that will log "the universe".
+// Write the code that will log the character "l" from theQuestion.
+// Write the code that will log whether theQuestion.length is greater than theAnswer.
