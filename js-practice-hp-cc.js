17cc15038a6c7d7c804ad14ec3ad9efc394fc661
holdenprine <holdenprine404@gmail.com>
2022-08-03 13:21:13 -0700

    test message

--- js-practice-hp-cc.js
+++ js-practice-hp-cc.js
@@ -45,3 +45,20 @@ console.log(myCohort.includes("rn"))
 console.log(myCohort.charAt(0) === "l")
 // Modify the code from the previous question to return true.
 console.log(myCohort.charAt(0) === "L")
+
+var theQuestion = "life, the universe, and everything"
+var theAnswer = 42
+// Write the code that will log theAnswer divided by 2.
+console.log(theAnswer / 2)
+// Write the code that will log the length of theQuestion.
+console.log(theQuestion.length)
+// Write the code that will log the index of the character "f" in the theQuestion.
+console.log(theQuestion.indexOf("f"))
+// Write the code that will log the concatenation of the two variables.
+console.log(theQuestion + theAnswer)
+// Write the code that will log "the universe".
+console.log(theQuestion.substring(6, 18))
+// Write the code that will log the character "l" from theQuestion.
+console.log(theQuestion.charAt(0))
+// Write the code that will log whether theQuestion.length is greater than theAnswer.
+console.log(theQuestion.length > theAnswer)
