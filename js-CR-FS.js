5ce24603842f699c87f7faeab1d1afe648c66784
roeckerc <roeckerc@gmail.com>
2022-08-03 15:55:52 -0700

    your turn

--- js-CR-FS.js
+++ js-CR-FS.js
@@ -10,9 +10,24 @@ if (price <= 100){
 }
 
 // Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+ var hungry = true
+ if ( hungry === true){
+    console.log("eat food")
+ } else {
+    console.log("keep coding")
+ }
 
 
 // Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+var trafficLight = "green"
+
+if (trafficLight === "green") {
+    console.log("go")
+} else if (trafficLight === "yellow") {
+    console.log("slow down!")
+} else if (trafficLight === "red") {
+    console.log("STOP!!!!")
+}
 
 // Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
 
