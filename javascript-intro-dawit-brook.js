6fc40a152a8380842a9c8ad4f69c98dffb01af8c
ResetReesi <brookmoore718@gmail.com>
2022-08-03 15:32:16 -0700

    Question number 3

--- javascript-intro-dawit-brook.js
+++ javascript-intro-dawit-brook.js
@@ -3,6 +3,7 @@ console.log(34 + 71)
 //Write the code that will log the outcome of 67 subtracted from 123.
 console.log(123 - 67)
 //Write the code that will log the outcome of 56 multiplied by 23.
+console.log(56*23)
 //Write the code that will log the outcome of 45 divided by 5.
 //Write the code that will log the outcome of 5 to the power of 7.
 //Write the code that will log the whole number remainder of 33 divided by 6.
