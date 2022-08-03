52c4c20d30564b2999dcb170c77b8e9d72d7d21a
kellychan56 <kelly.chan568@gmail.com>
2022-08-03 11:48:06 -0700

    next set

--- intro-kelly-cody.js
+++ intro-kelly-cody.js
@@ -18,3 +18,18 @@ console.log("Kelly".includes("e"));
 console.log("Kelly".charAt(0));
 // Write the code that will log the string in all uppercase letters.
 console.log("Kelly".toUpperCase());
+// Is 34 divided by 3 greater than 67 divided by 2?
+console.log(34 / 3 > 67 / 2 );
+// Does 5 evaluate to the same as "5"?
+console.log(5 == "5");
+// Does 5 strictly equal "5"?
+console.log(5 === "5");
+// Does !3 strictly equal 3?
+console.log(!3 === 3);
+// Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+console.log("LEARN".length === 5 && "Student".length === 7);
+// Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+// Does "LEARN" contain the subset "RN"?
+// Does "LEARN" contain the subset "rn"?
+// Does "LEARN"[0] strictly equal "l"?
+// Modify the code from the previous question to return true.
