dd6ed4d296671d423ff710be12b6f0e5d7218b4e
jmagistrado <joycemagistrado@gmail.com>
2022-08-03 13:41:17 -0700

    Here is our answers

--- challenges-ll-jm.js
+++ challenges-ll-jm.js
@@ -19,3 +19,42 @@ console.log(myName.includes("e"))
 console.log(myName.indexOf("j"))
 // Write the code that will log the string in all uppercase letters.
 console.log(myName.toUpperCase())
+// Is 34 divided by 3 greater than 67 divided by 2?
+console.log(34 / 3 > 67 / 2)
+// Does 5 evaluate to the same as "5"?
+console.log(5 == "5")
+// Does 5 strictly equal "5"?
+console.log(5 === "5")
+// Does !3 strictly equal 3?
+console.log(!3 === 3)
+// Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+console.log("LEARN".length === "Student".length)
+// Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+console.log("LEARN".length === 5 || "Student".length  === 10 )
+// Does "LEARN" contain the subset "RN"?
+var className = "LEARN"
+console.log(className.includes("RN"))
+// Does "LEARN" contain the subset "rn"?
+console.log(className.includes("rn"))
+// Does "LEARN"[0] strictly equal "l"?
+console.log("LEARN"[0] === "l")
+// Modify the code from the previous question to return true.
+console.log("LEARN"[0] === "L")
+
+var theQuestion = "life, the universe, and everything";
+var theAnswer = 42;
+
+// Write the code that will log theAnswer divided by 2.
+console.log(theAnswer / 2 )
+// Write the code that will log the length of theQuestion.
+console.log(theQuestion.length)
+// Write the code that will log the index of the character "f" in the theQuestion.
+console.log(theQuestion.indexOf("f"))
+// Write the code that will log the concatenation of the two variables.
+console.log(theQuestion +  theAnswer)
+// Write the code that will log "the universe".
+console.log(theQuestion.slice(5,18))
+// Write the code that will log the character "l" from theQuestion.
+console.log(theQuestion.charAt(0))
+// Write the code that will log whether theQuestion.length is greater than theAnswer.
+console.log(theQuestion > theAnswer)
