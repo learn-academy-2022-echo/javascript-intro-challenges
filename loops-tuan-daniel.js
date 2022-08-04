ee117f1d341702ffd2ee5cd0db8b32896f2fef12
dnlclmnt <dnlclmnt@gmail.com>
2022-08-04 15:54:22 -0700

    completed one more set of questions

--- loops-tuan-daniel.js
+++ loops-tuan-daniel.js
@@ -45,3 +45,33 @@ for (let i = 0; i < nums.length; i++) {
     console.log(nums[i] % 2)
 }
 // Expected output: 1, 1, -1, 0, 1
+//////////////////////////////////////
+// Consider this variable:
+
+const myString = "learn student";
+
+// Create the code that will log the number of times the letter "e" occurs in the string.
+let numOfE = 0;
+for (let i = 0; i < myString.length; i++) {
+    if (myString[i] === "e") {
+        numOfE++
+    }
+}
+console.log(numOfE);
+// Create the code that will log every other character in the string.
+for (let i = 0; i < myString.length; i = i + 2) {
+    console.log(myString[i])
+}
+
+// STRETCH Challenges
+// Create the code that iterates from 5 to 15. For each iteration log if the current number is odd or even.
+for (let i = 5; i < 16; i++) {
+    if (i % 2 === 0) {
+        console.log(`${i} is EVEN`)
+    } else {
+        console.log(`${i} is ODD`)
+    }
+}
+// Expected output: "5 is odd" "6 is even" "7 is odd" ...etc
+// Fizz Buzz: Create the code that will iterate from 1-100. If a number is a multiple of 3, replace it with the word fizz. If a number is a multiple of five, replace it with the word buzz. If a number is a multiple of both 3 and 5, replace it with fizzbuzz.
+// Expected output: 1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz" ...etc
