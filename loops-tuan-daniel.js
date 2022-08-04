c5153e55eb59d65fbeb52a0b327570186890811d
TuanLeVietz <tuanz_z@yahoo.com>
2022-08-04 15:10:32 -0700

    loops tuan

--- loops-tuan-daniel.js
+++ loops-tuan-daniel.js
@@ -21,3 +21,27 @@ for (let i = 1; i < 21; i = i + 2) {
         }
     }
 // Expected output: ODD, 2, ODD, 4, ODD, 6 ...etc
+
+const nums = [3, 57, -9, 20, 67];
+// Create the code that will log the largest number from the array.
+var largest = nums[0]
+for (let i = 1; i< nums.length; i++){
+    if(nums[i] > largest){
+        largest = nums[i]
+    }
+}
+console.log(largest)
+// Create the code that will log the smallest number from the array.
+var smallest = nums[0]
+for (let i=1; i<nums.length; i++){
+    if(nums[i] < smallest){
+        smallest = nums[i]
+    }
+}
+console.log(smallest)
+// Create the code that will log the remainder of each number when divided by 2.
+
+for (let i = 0; i < nums.length; i++) {
+    console.log(nums[i] % 2)
+}
+// Expected output: 1, 1, -1, 0, 1
