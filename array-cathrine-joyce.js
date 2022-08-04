7d0423f6e967707b5c3ca391773ee9236889af13
jmagistrado <joycemagistrado@gmail.com>
2022-08-04 11:35:01 -0700

    Here is our second portion of questions

--- array-cathrine-joyce.js
+++ array-cathrine-joyce.js
@@ -1,43 +1,55 @@
 // Consider the variable:
 
-var groceryList = ["chips", "dip", "cookies"];
-// Write the code that will add "soda" to the end of the original array.
-console.log(groceryList.push("soda"))
-console.log(groceryList)
+// var groceryList = ["chips", "dip", "cookies"];
+// // Write the code that will add "soda" to the end of the original array.
+// console.log(groceryList.push("soda"))
+// console.log(groceryList)
 
-// Write the code that will add "granola" to the end of array without altering the original array.
-console.log(groceryList.pop())
-console.log(groceryList.push("granola"))
-console.log(groceryList)
+// // Write the code that will add "granola" to the end of array without altering the original array.
+// console.log(groceryList.pop())
+// console.log(groceryList.push("granola"))
+// console.log(groceryList)
 
-// Write the code that will return a subset of the array containing only "chips" and "dip".
-console.log(groceryList.slice(0, 2))
+// // Write the code that will return a subset of the array containing only "chips" and "dip".
+// console.log(groceryList.slice(0, 2))
 
-// Write the code that will add "beans" to the "chips" and "dip" array.
-console.log(groceryList.pop())
-console.log(groceryList.pop())
-console.log(groceryList.push("beans"))
-console.log(groceryList)
+// // Write the code that will add "beans" to the "chips" and "dip" array.
+// console.log(groceryList.pop())
+// console.log(groceryList.pop())
+// console.log(groceryList.push("beans"))
+// console.log(groceryList)
 
 // Consider the variable:
 
 // var numbers = [2, 4, 6, 8, 10];
-// Write the code that will add the number 0 to the beginning of the array.
+// // Write the code that will add the number 0 to the beginning of the array.
+// console.log(numbers.unshift(0))
+// console.log(numbers)
 
-// Write the code that will add the number 12 to the end of the array.
+// // Write the code that will add the number 12 to the end of the array.
+// console.log(numbers.push(12))
+// console.log(numbers)
 
-// Write the code that will remove the first number from the array.
+// // Write the code that will remove the first number from the array.
+// console.log(numbers.shift())
+// console.log(numbers)
 
-// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+// // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+// console.log(numbers.splice(0,0,0))
+// console.log(numbers)
 
 // Consider the variable:
 
-// var numSet = [2, 13, 6, 8, 4, 2];
+var numSet = [2, 13, 6, 8, 4, 2];
 // Write the code that finds the index of the first appearance of the number 2.
+console.log(numSet.indexOf(2))
+
 
 // Write the code that finds the index of the last appearance of the number 2.
+console.log(numSet.lastIndexOf(2))
 
 // Write the code that returns the number at the third index.
+console.log(numSet.slice(3,4))
 
 // Consider the variable:
 
