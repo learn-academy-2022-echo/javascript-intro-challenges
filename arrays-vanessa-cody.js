a6c9ade868d907bb189bae9bd3da0fdcfd4c86d0
VKathryn <vkbastien@gmail.com>
2022-08-04 11:39:25 -0700

    next four questions

--- arrays-vanessa-cody.js
+++ arrays-vanessa-cody.js
@@ -17,4 +17,25 @@ console.log(grocerySubSet);
 console.log(grocerySubSet.push("beans"));
 console.log(grocerySubSet);
 
+// Consider the variable:
+
+var numbers = [2, 4, 6, 8, 10];
+// Write the code that will add the number 0 to the beginning of the array.
+    console.log(numbers.unshift(0));
+    console.log(numbers);
+// Write the code that will add the number 12 to the end of the array.
+    console.log(numbers.push(12));
+    console.log(numbers);
+// Write the code that will remove the first number from the array.
+    console.log(numbers.shift(0));
+    console.log(numbers);
+// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+    var zero = [0];
+    var numbersOne = console.log(zero.concat(numbers));
+    var newArray = zero + numbers
+    console.log(newArray.split(","));
+    var spreadArray = [0,...numbers].flat()
+    console.log(spreadArray);
+
+    
 
