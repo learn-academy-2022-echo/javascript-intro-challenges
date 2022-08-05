628906da8f0c80be9487a3de572d2e32d0d5262a
HeavyArms0511 <learnacademy@LEARNs-MacBook-Air.local>
2022-08-05 11:13:24 -0700

    First 5 questions

---
+++ functions-tuan-cody.js
@@ -0,0 +1,58 @@
+// Write a function named marco that returns "polo".
+
+// // create a function called marco and have it return polo
+// const marco = () =>{
+//     return "polo"
+// }
+// console.log(marco());
+
+// Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+
+//create a functon named greeting that takes in your name and returns "Welcome, <person's name here>!"
+
+// const greeting = (yourName) => {
+// return `Welcome, ${yourName}`
+// }
+// console.log(greeting("cody"));
+
+// Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
+
+// create a function named oddOrEven that takes in a number called number
+// const oddOrEven = (number) =>{
+//     //if number is even, want it to return "this number is even"
+//     if(number % 2 === 0){
+//         return `${number} is even`
+//     }
+//     //if number is odd, want it to return "this number is odd"
+//     else if(number % 2 !== 0){
+//         return `${number} is odd`
+//     }
+// }
+// console.log(oddOrEven(567891));
+
+// Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+
+//create a function with the name triple that takes in a number and triples it
+// const triple = (num1) => {
+//     return (num1 * 3)
+// }
+// console.log(triple(3));
+  
+// Write a function named multiply that takes two numbers as arguments and returns the result of the numbers multiplied together.
+
+//create a function named multiply, takes 2 numbers as arguments and outputs them multiplied togehter
+
+const multiply = (num1, num2) =>{
+    return (num1 * num2)
+}
+console.log(multiply(5, 8));
+// Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+
+// Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
+
+// Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
+
+// Write a function named greaterNum that takes two numbers as arguments and returns whichever number is the greater (higher) number.
+
+// Write a function named yelling that takes a string as an argument and return the string in all uppercase case letters.
+
