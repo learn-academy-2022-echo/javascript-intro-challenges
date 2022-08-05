909b11b84a64af48858e787523dcef50ce37b605
namieralsabagh <namier@me.com>
2022-08-05 12:53:06 -0700

    updated code

---
+++ functions-na-jb.js
@@ -0,0 +1,44 @@
+//Write a function named marco that returns "polo".
+// const marco = () => {
+//     return 'polo'
+// }
+// console.log(marco())
+
+//Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+
+// const greeting = (name) => {
+//     return `Welcome ${name}`
+// }
+// console.log(greeting("Namier"))
+
+//Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
+
+// const oddOrEven =  (number) => {
+// if(number == 0,2,4,6,8,10){
+    // return `Your number is even`
+// } else {
+    // return `Your number is odd`
+// }
+// }
+// console.log(oddOrEven(1))
+
+//Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+
+//const triple = (number) => {
+ //   return number * 3;  
+//}
+//console.log(triple(4))
+
+//Write a function named multiply that takes two numbers as arguments and returns the result of the numbers multiplied together.
+
+//const multiply = (numberOne, numberTwo) =>{
+ //   return numberOne * numberTwo;
+//}
+//console.log(multiply(3,4))
+
+//Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+
+const divisibleBy = (numberOne, numberTwo) =>{
+    return numberOne / numberTwo 
+
+}
