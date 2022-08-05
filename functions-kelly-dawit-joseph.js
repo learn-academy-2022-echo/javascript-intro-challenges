e98f355a8c22c039f9e7fb7898691b159fbd12f0
kellychan56 <kelly.chan568@gmail.com>
2022-08-05 11:11:58 -0700

    1st 3 function challenges

---
+++ functions-kelly-dawit-joseph.js
@@ -0,0 +1,48 @@
+// Write a function named marco that returns "polo".
+    // create a function named marco
+    // return "polo"
+    const marco = () => {
+        return "polo"
+    }
+    console.log(marco());
+// Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+    // create a function named greeting
+    // have an argument called name
+    // return a message with the person's name
+    const greeting = (name) => {
+        return `Welcome, ${name}!`
+    }
+    console.log(greeting("Kelly"));
+// Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
+    // create a function named oddOrEven
+    // argument is called number
+    // make an if/else statement that determins if ther number is odd or even
+    const oddOrEven = (number) => {
+        if (number % 2 !== 0) {
+            return `The result: ${number} is odd`
+        } else if (number % 2 !== 1) {
+            return `The result: ${number} is even`
+        }
+    }
+    console.log(oddOrEven(355));
+// Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+
+// Write a function named multiply that takes two numbers as arguments and returns the result of the numbers multiplied together.
+
+// Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+
+// Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
+
+// Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
+
+// Write a function named greaterNum that takes two numbers as arguments and returns whichever number is the greater (higher) number.
+
+// Write a function named yelling that takes a string as an argument and return the string in all uppercase case letters.
+
+// STRETCH Challenges
+// The World Translator
+// (a) Write a function named helloWorld that takes a language code (e.g. "es", "de", "en") as an argument and returns "Hello World!" in the given language. Ensure you function works for at least 5 languages.
+// (b) Have your function default to returning English.
+
+// The Pluralizer
+// (a) Write a function named pluralizer that takes a number and a singular noun as arguments and returns the number and pluralized form of the noun, if necessary.
