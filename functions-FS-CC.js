6203b696422227116f23227918776ada6980c40b
CathrineC <cjsr.carreon@gmail.com>
2022-08-05 11:35:18 -0700

    testing-changes

--- functions-FS-CC.js
+++ functions-FS-CC.js
@@ -38,6 +38,7 @@
 //     return `${firstNum * secondNum}`
 // } 
 // console.log(multiply(6, 7))
+ 
 
 // Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
 
