2573eb614816760ad19033aaf03c37631a5238c0
luislaurel <luislaurel133@gmail.com>
2022-08-04 18:02:14 -0600

    Loops Updated

--- javascriptloops-luis-joseph.js
+++ javascriptloops-luis-joseph.js
@@ -21,31 +21,38 @@
 //}
 
 //Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".
-var challenge1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-for (let i = 0; i < challenge1.length; i++) {
-      if(challenge1[i] % 2 !== 1){
-       console.log(challenge1[i]);
-      } else { (challenge1[i] % 2 !== 0) 
-        console.log("Odd")
-       }
-   }
+// var challenge1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+// for (let i = 0; i < challenge1.length; i++) {
+//       if(challenge1[i] % 2 !== 1){
+//        console.log(challenge1[i]);
+//       } else { (challenge1[i] % 2 !== 0) 
+//         console.log("Odd")
+//        }
+//    }
 //Expected output: ODD, 2, ODD, 4, ODD, 6 ...etc
 
 //Consider this variable:
-//const nums = [3, 57, -9, 20, 67];
+const nums = [3, 57, -9, 20, 67];
 
 //Create the code that will log the largest number from the array.
-
+// for (let i = 0; i < nums.length ; i++)
+//     if (nums[i] > 66)
+//     console.log(nums[i])
 //Create the code that will log the smallest number from the array.
-
+// for (let i = 0; i < nums.length ; i++)
+//     if (nums[i] < 3)
+//     console.log(nums[i])
 //Create the code that will log the remainder of each number when divided by 2.
-
+// for (let i = 0; i < nums.length ; i++)
+//     console.log(nums[i] % 2)
 //Expected output: 1, 1, -1, 0, 1
 
 //Consider this variable:
 
-//const myString = "learn student";
+const myString = "learn student";
 //Create the code that will log the number of times the letter "e" occurs in the string.
+// for (let i = 0; i < myString.length ; i++)
+console.log(myString.split("e").length - 1)
 
 //Create the code that will log every other character in the string.
 
